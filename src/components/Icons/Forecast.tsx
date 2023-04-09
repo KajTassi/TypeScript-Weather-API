2df78b9233fc969a0ca01223150a268f982880d3
Kaj <tassi100@mail.chapman.edu>
2023-04-09 11:46:24 -0700

    small changes

--- src/components/Icons/Forecast.tsx
+++ src/components/Icons/Forecast.tsx
@@ -1,12 +1,16 @@
 import { forecastType } from "../../types"
 
+//referenced in index.ts
 type Props = {
     data: forecastType
 }
+
 const Forecast = ({ data }: Props): JSX.Element => {
 
     return (
-        <div>
+        <div className="w-full md:max-w-[500px] py-4 md:py-4
+        md:px-10 lg:px-24 h-full lg:h-auto bg-white
+        bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg">
             <p>Forecast</p>
         </div>
     )
